eb25587cefad46cf53f39e4b1d4b02d5ceac46c8
Allyson Reis <allysonreis1@hotmail.com>
2019-03-03 16:21:12 -0300

    Adding the user interection

--- TaxGovernment/Program.cs
+++ TaxGovernment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using TaxGovernment.Entities;
 
 namespace TaxGovernment
 {
@@ -6,7 +8,40 @@ namespace TaxGovernment
     {
         static void Main(string[] args)
         {
-            
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company(i / c) ? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual Income: ");
+                double anualIcome = double.Parse(Console.ReadLine());
+
+                if (ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+                    list.Add(new Individual(name, anualIcome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIcome, numberOfEmployees));
+                }
+            }
+
+            foreach (TaxPayer taxPayer in list)
+            {
+                Console.WriteLine(taxPayer.Tax());
+            }
         }
     }
 }
