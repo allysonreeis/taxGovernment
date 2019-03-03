28172e091a16fcda73967322611042766df4c024
Allyson Reis <allysonreis1@hotmail.com>
2019-03-03 15:58:12 -0300

    Adding an abstract method

--- TaxGovernment/Entities/Individual.cs
+++ TaxGovernment/Entities/Individual.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TaxGovernment.Entities
+﻿namespace TaxGovernment.Entities
 {
-    class Individual
+    class Individual : TaxPayer
     {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base (name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            double total = 0.0;
+            if (AnualIncome < 20000.00)
+            {
+                total = (AnualIncome * 0.15) - (HealthExpenditures * 0.5);
+            }
+            else
+            {
+                total = (AnualIncome * 0.25) - (HealthExpenditures * 0.5);
+            }
+
+            return total;
+        }
     }
 }
