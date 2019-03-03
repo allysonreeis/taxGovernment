182a65afc37c9c6a4a108f04dc3fce4d06ed2f25
Allyson Reis <allysonreis1@hotmail.com>
2019-03-03 16:01:33 -0300

    Creating a new class Company

---
+++ TaxGovernment/Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace TaxGovernment.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company (string name, double anualIncome, int numberOfEmployees) : base (name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+    }
+}
